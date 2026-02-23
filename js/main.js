df021717099cb315f6f3131dcef7b9fc72e6c456
gerardg24 <gerardgou24@gmail.com>
2026-02-23 17:42:27 +0100

    He modificat l'estil visual i he implementat la funcio de jugar, la qual ara demana un nom i el mostra per pantalla.

--- js/main.js
+++ js/main.js
@@ -1,21 +1,27 @@
 addEventListener('load', function() {
-    document.getElementById('play').addEventListener('click', 
-    function(){
-        alert("Comença la partida");
+    
+    document.getElementById('play').addEventListener('click', function(){
+        // Demanem l'àlies a l'usuari
+        let alias = prompt("Introdueix el teu àlies de jugador:");
+
+        if (alias !== null && alias !== "") {
+            console.log("Àlies del jugador: " + alias);
+            alert("Benvingut/da, " + alias + ". Comença la partida!");
+        } else {
+            console.log("El jugador no ha introduït cap àlies.");
+            alert("Has de posar un nom per jugar!");
+        }
     });
 
-    document.getElementById('options').addEventListener('click', 
-    function(){
+    document.getElementById('options').addEventListener('click', function(){
         console.error("Opció no implementada");
     });
 
-    document.getElementById('saves').addEventListener('click', 
-    function(){
+    document.getElementById('saves').addEventListener('click', function(){
         console.error("Opció no implementada");
     });
 
-    document.getElementById('exit').addEventListener('click', 
-    function(){
+    document.getElementById('exit').addEventListener('click', function(){
         console.warn("No es pot sortir!");
     });
 });
