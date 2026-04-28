46208159aa02d6faf04c9c5ac7a5c6dd972185bc
gerardg24 <gerardgou24@gmail.com>
2026-04-28 03:12:04 +0200

    Actualitza la lògica de les opcions per als modes de joc

--- js/options.js
+++ js/options.js
@@ -4,19 +4,14 @@ addEventListener('load', function() {
     let startLevelInput = document.getElementById('startLevel');
     let saveBtn = document.getElementById('save');
 
-    numCardsInput.value = sessionStorage.getItem('numCards') || localStorage.getItem('numCards') || 2;
-    groupSizeSelect.value = sessionStorage.getItem('groupSize') || localStorage.getItem('groupSize') || 2;
-    startLevelInput.value = sessionStorage.getItem('startLevel') || localStorage.getItem('startLevel') || 1;
+    numCardsInput.value = localStorage.getItem('numCards') || 2;
+    groupSizeSelect.value = localStorage.getItem('groupSize') || 2;
+    startLevelInput.value = localStorage.getItem('startLevel') || 1;
 
     saveBtn.addEventListener('click', function() {
-        sessionStorage.setItem('numCards', numCardsInput.value);
-        sessionStorage.setItem('groupSize', groupSizeSelect.value);
-        sessionStorage.setItem('startLevel', startLevelInput.value);
-
         localStorage.setItem('numCards', numCardsInput.value);
         localStorage.setItem('groupSize', groupSizeSelect.value);
         localStorage.setItem('startLevel', startLevelInput.value);
-
         window.location.assign("../index.html");
     });
 });
